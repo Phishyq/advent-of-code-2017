bb08de809ef2c9984e3af847854043e8112ea55c
Phishyq <phishyq@gmail.com>
2017-12-04 20:24:39 +0000

    Day 4
    Solution + tests for day 4.

---
+++ AdventOfCode2017/AdventOfCode2017/Day4Solution.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2017
+{
+    public class Day4Solution
+    {
+        public IEnumerable<string> GetInputFromFile()
+        {
+            return File.ReadLines("../../PuzzleInputs/Day4.txt");
+        }
+
+        #region Part 1
+
+        // A new system policy has been put in place that requires all accounts to use a passphrase instead of simply a password.
+        // A passphrase consists of a series of words (lowercase letters) separated by spaces. 
+        // To ensure security, a valid passphrase must contain no duplicate words. For example:
+
+        //  aa bb cc dd ee is valid.
+        //  aa bb cc dd aa is not valid - the word aa appears more than once.
+        //  aa bb cc dd aaa is valid - aa and aaa count as different words.
+
+        // The system's full passphrase list is available as your puzzle input. How many passphrases are valid?
+
+        public int SolvePart1(IEnumerable<string> inputStrings)
+        {
+            return FindValidPassphrases(inputStrings);
+        }
+
+        private int FindValidPassphrases(IEnumerable<string> passphraseList)
+        {
+            int validPassphraseCount = 0;
+
+            foreach (var passphrase in passphraseList)
+            {
+                List<string> splitPhrase = passphrase.Split(' ').ToList();
+                validPassphraseCount += (splitPhrase.Count == splitPhrase.Distinct().Count()) ? 1 : 0;
+            }
+
+            return validPassphraseCount;
+        }
+
+        #endregion
+
+        #region Part 2
+
+        // For added security, yet another system policy has been put in place.
+        // Now, a valid passphrase must contain no two words that are anagrams of each other - that is,
+        // a passphrase is invalid if any word's letters can be rearranged to form any other word in the passphrase.
+        // For example:
+
+        //  abcde fghij is a valid passphrase.
+        //  abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+        //  a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+        //  iiii oiii ooii oooi oooo is valid.
+        //  oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+
+        //Under this new system policy, how many passphrases are valid?
+
+        public int SolvePart2(IEnumerable<string> inputStrings)
+        {
+            return FindValidPassphrasesIncludingAnagrams(inputStrings);
+        }
+
+        private int FindValidPassphrasesIncludingAnagrams(IEnumerable<string> passphraseList)
+        {
+            int validPassphraseCount = 0;
+
+            foreach (var passphrase in passphraseList)
+            {
+                List<string> splitPhrase = passphrase.Split(' ').ToList();
+                var sortedPhrases = new List<string>();
+
+                foreach(var phrase in splitPhrase)
+                {
+                    sortedPhrases.Add(new string(phrase.OrderBy(c => c).ToArray()));
+                }
+
+                validPassphraseCount += (sortedPhrases.Count == sortedPhrases.Distinct().Count()) ? 1 : 0;
+            }
+
+            return validPassphraseCount;
+        }
+
+        #endregion
+    }
+}
---
+++ AdventOfCode2017/AdventOfCode2017Tests/Day4Tests.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode2017;
+using System.Collections.Generic;
+
+namespace AdventOfCode2017Tests
+{
+    [TestClass]
+    public class Day4Tests
+    {
+        private Day4Solution _itemUnderTest;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _itemUnderTest = new Day4Solution();
+        }
+
+        #region Part 1 Tests
+
+        /// <summary>
+        /// aa bb cc dd ee is valid.
+        /// </summary>
+        [TestMethod]
+        public void Day4_Part1_Valid()
+        {
+            // Arrange
+            var testInput = new List<string> { "aa bb cc dd ee" };
+
+            // Act
+            var result = _itemUnderTest.SolvePart1(testInput);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        /// <summary>
+        /// aa bb cc dd aa is not valid - the word aa appears more than once.
+        /// </summary>
+        [TestMethod]
+        public void Day4_Part1_Invalid()
+        {
+            // Arrange
+            var testInput = new List<string> { "aa bb cc dd aa" };
+
+            // Act
+            var result = _itemUnderTest.SolvePart1(testInput);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        /// <summary>
+        /// aa bb cc dd aaa is valid - aa and aaa count as different words.
+        /// </summary>
+        [TestMethod]
+        public void Day4_Part1_ValidPartialMatch()
+        {
+            // Arrange
+            var testInput = new List<string> { "aa bb cc dd aaa" };
+
+            // Act
+            var result = _itemUnderTest.SolvePart1(testInput);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        #endregion
+
+        #region Part 2 Tests
+
+        /// <summary>
+        /// abcde fghij is a valid passphrase.
+        /// </summary>
+        [TestMethod]
+        public void Day4_Part2_Valid()
+        {
+            // Arrange
+            var testInput = new List<string> { "abcde fghij" };
+
+            // Act
+            var result = _itemUnderTest.SolvePart2(testInput);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        /// <summary>
+        /// abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+        /// </summary>
+        [TestMethod]
+        public void Day4_Part2_Invalid()
+        {
+            // Arrange
+            var testInput = new List<string> { "abcde xyz ecdab" };
+
+            // Act
+            var result = _itemUnderTest.SolvePart2(testInput);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        /// <summary>
+        /// a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+        /// </summary>
+        [TestMethod]
+        public void Day4_Part2_ValidPartialMatch()
+        {
+            // Arrange
+            var testInput = new List<string> { "a ab abc abd abf abj" };
+
+            // Act
+            var result = _itemUnderTest.SolvePart2(testInput);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        /// <summary>
+        /// iiii oiii ooii oooi oooo is valid.
+        /// </summary>
+        [TestMethod]
+        public void Day4_Part2_ValidNoMatches()
+        {
+            // Arrange
+            var testInput = new List<string> { "iiii oiii ooii oooi oooo" };
+
+            // Act
+            var result = _itemUnderTest.SolvePart2(testInput);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        /// <summary>
+        /// oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+        /// </summary>
+        [TestMethod]
+        public void Day4_Part2_InvalidMultipleMatches()
+        {
+            // Arrange
+            var testInput = new List<string> { "oiii ioii iioi iiio" };
+
+            // Act
+            var result = _itemUnderTest.SolvePart2(testInput);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        #endregion
+    }
+}
